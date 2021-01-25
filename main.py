dad9184602f0099addf17787b875c085165af773
tgsher9329 <76851983+tgsher9329@users.noreply.github.com>
2021-01-25 10:45:48 -0500

    updating and adding files

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
+from color import *
 import json
-# from typing import Counter
+# import sys
 from tabulate import tabulate
-# dictionary and habits that will eventually be saved into its own file, but
-# this is just to get it going for now
-# implement terminaltables
-# data_file = open("dataFile.txt", "r+")
-# make the menu put it in a while loop with a quit option
+# from Color_Console import *
+# maybe try color-console^^^^^^^^to add color to text
 
+# def colorize(table, month):
+#     blackOnGreen = "\x1b[1;30;42m"
+#     blackOnRed = "\x1b[1;30;41m"
+#     reset = "\x1b[0m"
+
+#     for day in table.keys():
+#         x = 0
+#         while x <  len(table[day]):
+#             if table[day][x] == "YES":
+#                 table[day][x] = (blackOnGreen + "YES" + reset)
+#             elif table[day][x] == "NO":
+#                 table[day][x] = (blackOnRed + "NO" + reset)
+#             x += 1
+
+# opens the two files we will be using to be able to read them
 with open('yearData.json', 'r') as yearData:
     year = json.load(yearData)
 with open('habitData.json', 'r') as habitData:
     habits = json.load(habitData)
+
+    # make the menu put it in a while loop with a quit option
 while True:
-    # with open('dataFile.py', 'r+') as dataFile:
         print()
         print("Pick an option you would like to do. ")
         print("1. Log new day.")
@@ -23,9 +37,11 @@ while True:
 
         # log a new day
         if user_input == '1':
+            # checks to make sure that the habits dict is not empty
             if len(habits["habits"]) <= 0:
                 print('You need to add some habits before you can view them.')
 
+            # goes through the process of adding whether or not they did the habit for a certain day
             elif len(habits["habits"]) > 0:
                 month = input('What month do you want to look at? (1-12) ')
                 day = input('What day do you want to look at? (1-31) ')
@@ -39,33 +55,23 @@ while True:
                     that_habit = habits["habits"][i]
                     answer = input(f'Did you {that_habit}?("yes" or "no"')
                     answer = answer.upper()
-
-                    # changes colors
-                    # if answer == "YES":
-                    #     answer = (f"\033[1;30;42m {year[month]}")
-
-                    # elif answer == "NO":
-                    #     answer = (f"\033[1;30;41m {year[month]}")
-                    
                     year[month][day].append(answer)
 
                     # writes data to the other file
                     with open('yearData.json', 'w') as yearData:
                         json.dump(year, yearData)
 
+            # this happens when they do not 
             else:
                 print("It seems like you didnt enter a valid month and/or day")
 
-
-    # shows the user the entire month that they choose
+        # shows the user the entire month that they choose
         elif user_input == '2':
-            day = 1
-            month = input('What month do you want to look at? (1-12) ')
-            # if ((month >= 1 and month <= 12) and (len(habits) > 0)):
+            
+            month = input('\nWhat month do you want to look at? (1-12) ')
                 # print(year[month])
                 # ^^^^^^ this prints the entire dict, in a hard to read manner
             
-            # print(habits["habits"])
 
             # check to see if the month they want to see if there is anything in the days, if not tell them to enter a day, or multiple days
             # if the len of each day added together is longer than 0 then go to the table
@@ -77,31 +83,18 @@ while True:
                     length = length
                 
             
-        # tabulate and print table
+            # tabulate and print table
             table = year[month]
+
+            # adding color to the text in the table
             if ((int(month) >= 1 and int(month) <= 12) and (len(habits["habits"]) > 0) and (length > 0)):
+
+                # gets the colorize funciton from the other file and uses it here
+                colorize(table, month)
+
+
                 print('Keep in mind, it is going to output them in the order you input them.')
                 print(tabulate(table, headers = "keys", showindex = habits["habits"], tablefmt = "pretty"))
-                
-
-
-    # terminal tables
-            # if ((int(month) >= 1 and int(month) <= 12) and (len(habits["habits"]) > 0)):
-            #     table = AsciiTable()
-            #     print(table.table)
-
-
-    # just trying to manualy make table
-            # if ((int(month) >= 1 and int(month) <= 12) and (len(habits["habits"]) > 0)):
-            #     print("Day " *habits[habits], seq = ", ")
-
-
-
-    # OG just printing the dict
-            # if ((int(month) >= 1 and int(month) <= 12) and (len(habits["habits"]) > 0)):
-            #     for value in year[month]:
-            #         print(f'{day} : {year[month][value]}')
-            #         day += 1
                 
 
             else:
@@ -145,5 +138,6 @@ while True:
             print('Good luck')
             quit()
         
+        # tells the user that their input was invalid
         else:
             print('Invalid choice, choose another.')
